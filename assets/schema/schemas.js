a94dacbc06610844a5d821b0dc3da7a163889160
Bertrand Darbon <bertrand.darbon@gmail.com>
2016-04-23 01:04:21 +0200

    fix(schema): use new schema due to server changes

--- assets/schema/schemas.js
+++ assets/schema/schemas.js
@@ -1,10 +1,11 @@
-import { normalize, Schema } from 'normalizr'
+import { normalize, Schema, arrayOf } from 'normalizr'
 
 export const staticBuilding = new Schema('staticBuildings');
 export const user = new Schema('users');
 export const player = new Schema('players');
 export const base = new Schema('bases');
 
-base.define({
-    owner: player
+player.define({
+    bases: arrayOf(base),
+    currentBase: base
 });
