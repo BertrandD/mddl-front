070823ca48266ca849313387be9e88bc0f2bcbd2
Bertrand Darbon <bertrand.darbon@gmail.com>
2016-05-31 14:18:01 +0200

    feat(status): add hostname into status response to debug load balancer

--- src/status/index.js
+++ src/status/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+var os = require("os");
+
+var hostname = os.hostname();
 
 router.get('/', (req, res) => {
-  res.status(200).send();
+  res.status(200).send('<html><body>Hello from host ' + hostname + '</body></html>');
 });
 
 module.exports = router;
