583ff5cd1052b7ee9d3cf16ad9a6bc2352b77143
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-01-24 12:36:54 +0100

    add create ship logic

--- assets/modules/buildings/components/buildings/SpatialElevator.js
+++ assets/modules/buildings/components/buildings/SpatialElevator.js
@@ -10,20 +10,53 @@ class SpatialElevator extends Component {
 
     constructor(props, context) {
         super(props, context);
+        this.state = {number: 1, structure: ''};
+
+        this.handleStructureChange = this.handleStructureChange.bind(this);
+        this.handleNumberChange = this.handleNumberChange.bind(this);
+    }
+
+    handleStructureChange(event) {
+        this.setState({structure: event.target.value});
+    }
+
+    handleNumberChange(event) {
+        this.setState({number: event.target.value});
     }
 
     render() {
 
         const { sBuildings, sItems, strings } = this.props;
-
         return (
             <div>
                 <h3>Lancer la production de vaisseaux</h3>
-                {filter(sItems, (b) => b.type == ItemTypes.STRUCTURE).map((b) => (
-                    <div>
-                        {b.name}
-                    </div>
-                ))}
+                <div>
+                    Structure :&nbsp;
+                    <select value={this.state.structure} onChange={this.handleStructureChange}>
+                        <option value=""> </option>
+                        {filter(sItems, (b) => b.type == ItemTypes.STRUCTURE).map((b) => (
+                            <option key={b.itemId} value={b.itemId}>{b.name}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    Quantité :&nbsp;
+                    <input type="number"
+                           value={this.state.number}
+                           onChange={this.handleNumberChange}
+                           size="5"/>
+                </div>
+                <div>
+                    {this.state.structure && this.state.number > 0 && (
+                        <button className="button--primary" onClick={this.props.actions.createShip.bind(null, this.state.structure, this.state.number, [])}>
+                            Produire
+                        </button>
+                    ) || (
+                        <button className="button--primary" disabled>
+                            Produire
+                        </button>
+                    )}
+                </div>
             </div>
         )
     }
@@ -40,9 +73,10 @@ function mapStateToProps(state, ownProps) {
 }
 
 import { createModule, viewBuildingDetails } from 'actions/buildingActions'
+import { createShip } from 'actions/shipActions'
 
 function mapDispatchToProps(dispatch) {
-    return {actions: bindActionCreators({ createModule, viewBuildingDetails }, dispatch)}
+    return {actions: bindActionCreators({ createModule, viewBuildingDetails, createShip }, dispatch)}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SpatialElevator);
---
+++ core/actions/shipActions.js
@@ -0,0 +1,35 @@
+import * as ShipActions from './../actionTypes/ShipActionTypes';
+import { postAsForm, fetch } from '../utils/post-as-form'
+import config from '../config'
+import { fetchBaseSuccess, updateBase } from './baseActions'
+import { normalize, arrayOf } from 'normalizr'
+import { base } from 'schema/schemas'
+import { notify } from './appActions'
+
+function createShipSuccess(ship) {
+    return {
+        type: ShipActions.CREATE_SHIP_SUCCESS,
+        payload: {
+            ship
+        }
+    }
+}
+
+export function createShip(itemId, count, attachments) {
+    return dispatch => {
+        return postAsForm(config.api.url + '/ship', {
+            structureId: itemId,
+            count,
+            attachments
+        })
+            .catch(res => {
+                dispatch(notify(res.meta && res.meta.message ? res.meta.message : 'An error occured'));
+                return Promise.reject(res);
+            })
+            .then(res => {
+                dispatch(createShipSuccess(res.payload));
+                dispatch(fetchBaseSuccess(normalize(res.meta, base).entities));
+                dispatch(notify(count + ' ' + itemId + ' has been successfully created !'));
+            })
+    }
+}
